158405d43c7ca66dfe07a2d861a224579ed5da69
Aleksandr Chistyh <sashchystyh@yandex.ru>
2022-04-19 12:34:17 +0300

    refactor: wait command

--- api/management/commands/wait_for_db.py
+++ api/management/commands/wait_for_db.py
@@ -1,18 +1,18 @@
+import time
+
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
-import time
 
 
 class Command(BaseCommand):
-    """ Django command to pause execution until database is available"""
+    """Django command to pause execution until database is available"""
+
     def handle(self, *args, **kwargs):
-        self.stdout.write('waiting for db ...')
-        db_conn = None
-        while not db_conn:
+        db_connect = None
+        while not db_connect:
             try:
-                db_conn = connections['default']
-                self.stdout.write(self.style.SUCCESS('db available'))
+                db_connect = connections["default"]
             except OperationalError:
-                self.stdout.write("Database unavailable, waiting 1 second ...")
+                self.stdout.write("DB unavailable, waiting 1 second ...")
                 time.sleep(1)
